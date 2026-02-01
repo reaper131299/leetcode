8d79d8e6164123f5af9d1267561cd4bf412de015
ruyadav <ryadav27@ford.com>
2026-02-01 21:27:36 +0530

    Added 2 sum adn solitions file

---
+++ src/leetcode/ValidAnagram.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+        for(char x: s.toCharArray()){
+            freq[x-'a']++;
+        }
+        for(char y: t.toCharArray()){
+            freq[y-'a']--;
+        }
+        for(int x:freq){
+            if(x!=0)    return false;
+        }
+        return true;
+    }
+    
+}
